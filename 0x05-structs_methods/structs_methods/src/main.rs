8d1f820465eeb8dd05bec1c5300c374631b8adcb
wishon1 <wisdomhonest1@gmail.com>
2025-04-06 11:48:28 -0400

    add task 1

--- 0x05-structs_methods/structs_methods/src/main.rs
+++ 0x05-structs_methods/structs_methods/src/main.rs
@@ -6,5 +6,5 @@ fn main() {
     let rect = Rectangle::new(30, 50);
     println!("rect is {:?}", rect);
     println!("The area of the rectangle is {} square pixels.", rect.area());
-    println!("The area of the rectangle is {} square pixels.", rect.area());
+    
 }
