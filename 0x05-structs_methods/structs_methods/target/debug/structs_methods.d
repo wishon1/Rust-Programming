1172a5a3c7b576efcc42b03b7bfac94428583751
wishon1 <wisdomhonest1@gmail.com>
2025-04-06 11:46:24 -0400

    add empty repos

--- 0x05-structs_methods/structs_methods/target/debug/structs_methods.d
+++
@@ -1,1 +0,0 @@
-/home/pc/Rust-Programming/0x05-structs_methods/structs_methods/target/debug/structs_methods: /home/pc/Rust-Programming/0x05-structs_methods/structs_methods/src/main.rs /home/pc/Rust-Programming/0x05-structs_methods/structs_methods/src/rectangle.rs
