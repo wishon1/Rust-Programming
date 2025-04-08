db765cbe445a9357cc601ac8042da29d49bcdda0
wishon1 <wisdomhonest1@gmail.com>
2025-04-08 09:00:24 -0400

    add task 4

--- 0x06-enums_pattern_matching/_task-4_config/src/main.rs
+++ 0x06-enums_pattern_matching/_task-4_config/src/main.rs
@@ -1,3 +1,15 @@
+// 0x06. Rust - Enums and Pattern Matching
+use config::{ServerConfig, apply_config};
+
 fn main() {
-    println!("Hello, world!");
+    let configs = vec![
+        ServerConfig::IP(String::from("127.0.0.1")),
+        ServerConfig::Port(8080),
+        ServerConfig::MaxConnections(100),
+        ServerConfig::Port(9000),
+    ];
+    
+    for config in configs {
+        apply_config(config);
+    }
 }
